4d62ea38b9658979b4eb2ba8e5ab47c1c1c59213
Maja Ferle <maja.ferle@in516ht.com>
2025-01-27 11:40:21 +0100

    deploy scripts

--- search_service/Snowflake_objects/databases/APPLIANCES_DB/schemas/create_manuals.sql
+++ search_service/Snowflake_objects/databases/APPLIANCES_DB/schemas/create_manuals.sql
@@ -1,5 +1,5 @@
 -- create a schema
 use role sysadmin;
 create or alter schema appliances_db.manuals;
-grant all on schema manuals to role cortex_developer;
+grant all on schema appliances_db.manuals to role cortex_developer;
 use role cortex_developer;
