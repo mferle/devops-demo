7edf5f96ea162d95ba84e600f63e22dc350354f6
Maja Ferle <maja.ferle@in516ht.com>
2025-01-27 09:40:30 +0100

    deploy scripts

--- search_service/Snowflake_objects/deploy_objects.sql
+++ search_service/Snowflake_objects/deploy_objects.sql
@@ -1,7 +1,7 @@
-execute immediate from '../Snowflake_objects/databases/create_appliances_db.sql';
-execute immediate from '../Snowflake_objects/databases/schemas/create_manuals.sql';
-execute immediate from '../Snowflake_objects/databases/schemas/stages/create_manuals_stg.sql';
-execute immediate from '../Snowflake_objects/databases/schemas/tables/create_manuals_txt.sql';
+execute immediate from '../Snowflake_objects/databases/APPLIANCES_DB/create_appliances_db.sql';
+execute immediate from '../Snowflake_objects/databases/APPLIANCES_DB/schemas/MANUALS/create_manuals.sql';
+execute immediate from '../Snowflake_objects/databases/APPLIANCES_DB/schemas/MANUALS/stages/create_manuals_stg.sql';
+execute immediate from '../Snowflake_objects/databases/APPLIANCES_DB/schemas/MANUALS/tables/create_manuals_txt.sql';
 
 -- snow git fetch DEMO_REPO --database ADMIN_DB --schema GIT_INTEGRATION
 -- snow git list-branches DEMO_REPO --database ADMIN_DB --schema GIT_INTEGRATION
