b0a3a09dfc5f99ed8c9b0cea3b8fa1773bf85d00
Maja Ferle <maja.ferle@in516ht.com>
2025-01-27 13:12:01 +0100

    deploy scripts

--- search_service/Snowflake_objects_with_params/deploy_objects.sql
+++ search_service/Snowflake_objects_with_params/deploy_objects.sql
@@ -1,10 +1,10 @@
-execute immediate from '../Snowflake_objects_with_params/databases/APPLIANCES_DB/create_appliances_db.sql' USING (env=>'TST';
-execute immediate from '../Snowflake_objects_with_params/databases/APPLIANCES_DB/schemas/create_manuals.sql' USING (env=>'TST';
-execute immediate from '../Snowflake_objects_with_params/databases/APPLIANCES_DB/schemas/MANUALS/stages/create_manuals_stg.sql' USING (env=>'TST';
-execute immediate from '../Snowflake_objects_with_params/databases/APPLIANCES_DB/schemas/MANUALS/stages/create_snowpark_stg.sql' USING (env=>'TST';
-execute immediate from '../Snowflake_objects_with_params/databases/APPLIANCES_DB/schemas/MANUALS/tables/create_manuals_txt.sql' USING (env=>'TST';
+SET ENV = '''TST''';
+
+execute immediate from '../Snowflake_objects_with_params/databases/APPLIANCES_DB/create_appliances_db.sql' USING (env => $ENV);
+execute immediate from '../Snowflake_objects_with_params/databases/APPLIANCES_DB/schemas/create_manuals.sql' USING (env => $ENV);
+execute immediate from '../Snowflake_objects_with_params/databases/APPLIANCES_DB/schemas/MANUALS/stages/create_manuals_stg.sql' USING (env => $ENV);
+execute immediate from '../Snowflake_objects_with_params/databases/APPLIANCES_DB/schemas/MANUALS/stages/create_snowpark_stg.sql' USING (env => $ENV);
+execute immediate from '../Snowflake_objects_with_params/databases/APPLIANCES_DB/schemas/MANUALS/tables/create_manuals_txt.sql' USING (env => $ENV);
 
 -- snow git fetch DEMO_REPO --database ADMIN_DB --schema GIT_INTEGRATION
--- snow git list-branches DEMO_REPO --database ADMIN_DB --schema GIT_INTEGRATION
--- snow git list-files @DEMO_REPO/branches/main/ --database ADMIN_DB --schema GIT_INTEGRATION
--- snow git execute @DEMO_REPO/branches/main/search_service/Snowflake_objects/deploy_objects.sql --database ADMIN_DB --schema GIT_INTEGRATION
+-- snow git execute @DEMO_REPO/branches/main/search_service/Snowflake_objects_with_params/deploy_objects.sql --database ADMIN_DB --schema GIT_INTEGRATION
