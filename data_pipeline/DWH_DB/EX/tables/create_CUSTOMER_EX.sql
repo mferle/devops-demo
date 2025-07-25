83b1da218b58eb06671d2bb02df2e522c8c952f3
Maja Ferle <maja.ferle@in516ht.com>
2025-07-25 13:32:20 +0200

    update customer

--- data_pipeline/DWH_DB/EX/tables/create_CUSTOMER_EX.sql
+++ data_pipeline/DWH_DB/EX/tables/create_CUSTOMER_EX.sql
@@ -1,3 +1,4 @@
 use role data_pipeline_developer;
 use database dwh_db;
-create or replace table ex.customer_ex as select * from SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.CUSTOMER;
+create or replace table ex.customer_ex as 
+select * from SNOWFLAKE_SAMPLE_DATA.TPCH_SF10.CUSTOMER;
